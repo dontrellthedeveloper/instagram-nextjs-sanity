5519fe6c2690340187aa432cebac32a5532e8dc8
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-08-18 14:23:50 -0400

    started building the post component

---
+++ components/Post.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Post({id, username, userImg, caption}: any) {
+    return (
+        <div>
+            {/* Header */}
+
+
+            {/*  img  */}
+
+
+            {/*  Button  */}
+
+
+            {/*  caption  */}
+
+
+            {/*  comments  */}
+
+
+            {/*  input box  */}
+
+
+        </div>
+    );
+}
+
+export default Post;
