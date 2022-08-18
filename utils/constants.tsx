d85b134c83d1d2ce72c50464ed199136ccdd44bf
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-08-18 13:54:10 -0400

    added header, sanity client file, and sanity queries

---
+++ utils/constants.tsx
@@ -0,0 +1,54 @@
+import {BsBrush, BsCode, BsEmojiSunglasses, BsGlobe2} from 'react-icons/bs';
+import { GiCakeSlice, GiGalaxy, GiLipstick } from 'react-icons/gi';
+import { FaPaw, FaMedal, FaGamepad, FaPaintBrush } from 'react-icons/fa';
+import {RiShirtLine} from "react-icons/ri";
+import {AiOutlineCamera} from "react-icons/ai";
+import {IoGameControllerOutline, IoMedalOutline, IoPizzaOutline} from "react-icons/io5";
+
+export const topics = [
+
+  // {
+  //   name: 'comedy',
+  //   icon: <BsEmojiSunglasses />,
+  // },
+  {
+    name: 'photography',
+    icon: <AiOutlineCamera />,
+  },
+  {
+    name: 'gaming',
+    icon: <IoGameControllerOutline />,
+  },
+  {
+    name: 'food',
+    icon: <IoPizzaOutline />,
+  },
+  {
+    name: 'dance',
+    icon: <BsGlobe2 />,
+  },
+  {
+    name: 'beauty',
+    icon: <GiLipstick />,
+  },
+  // {
+  //   name: 'animals',
+  //   icon: <FaPaw />,
+  // },
+  {
+    name: 'fashion',
+    icon: <RiShirtLine />,
+  },
+  {
+    name: 'art',
+    icon: <BsBrush />,
+  },
+  {
+    name: 'sports',
+    icon: <IoMedalOutline />,
+  },
+];
+
+export const footerList1 = ['About', 'Newsroom', 'Store', 'Contact', 'Careers', 'ByteDance', 'Creator Directory']
+export const footerList2 = [ 'TikTik for Good','Advertise','Developers','Transparency','TikTik Rewards' ]
+export const footerList3 = [ 'Help', 'Safety', 'Terms', 'Privacy', 'Creator Portal', 'Community Guidelines' ]
