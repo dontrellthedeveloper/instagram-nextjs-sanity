f8174b46bec6cd82711b55785a5da3d95a372ab4
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-08-18 14:17:03 -0400

    added the stories section of the feed component

---
+++ components/Stories.tsx
@@ -0,0 +1,49 @@
+import React, {useEffect, useState} from 'react';
+import { faker } from '@faker-js/faker';
+import Story from "./Story";
+
+
+function Stories() {
+    const [suggestions, setSuggestions]: any = useState([]);
+
+    useEffect(()=> {
+        const suggestions = [...Array(20)].map((_, i) => ({
+            // ...faker.helpers.contextualCard(),
+            id: i,
+            name: faker.name.firstName(),
+            username: faker.internet.userName(),
+            avatar: faker.internet.avatar(),
+            email: faker.internet.email(),
+            dob: faker.date.birthdate(),
+            phone: faker.phone.number(),
+            address: {
+                street: faker.address.streetAddress(false),
+                suite: faker.address.secondaryAddress(),
+                city: faker.address.city(),
+                zipcode: faker.address.zipCode(),
+                state: faker.address.state(),
+                geo: faker.address.nearbyGPSCoordinate()
+            },
+            website: faker.internet.domainName(),
+            company: {
+                name: faker.company.companyName(),
+                catchPhrase: faker.company.catchPhraseNoun(),
+                bs: faker.company.bs()
+            }
+        }));
+
+        console.log(suggestions);
+        setSuggestions(suggestions);
+    },[])
+    return (
+        <div className='flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black'>
+            {suggestions.map((profile: any) => (
+                <Story key={profile.id} img={profile.avatar} username={profile.username}
+                />
+            ))}
+
+        </div>
+    );
+}
+
+export default Stories;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from 'next';
 import Head from "next/head";
 import Header from "../components/Header";
+import Feed from "../components/Feed";
 
 
 
 const Home: NextPage = () => {
     return (
-        <div className="">
+        <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
             <Head>
                 <title>Instagram | Dontrell Dev</title>
                 <link rel="icon" href="/favicon.ico" />
@@ -16,6 +17,7 @@ const Home: NextPage = () => {
             <Header/>
 
             {/* Feed */}
+            <Feed/>
 
             {/* Modal */}
         </div>
