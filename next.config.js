57c6fd348e2c2d05c0c88c2e47bee906f916da1b
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2022-10-26 23:51:57 -0400

    updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    domains: ['links.papareact.com' , 'i.ibb.co', 'lh3.googleusercontent.com']
+    domains: ['links.papareact.com' , 'i.ibb.co', 'lh3.googleusercontent.com', 'cdn.sanity.io']
   },
   reactStrictMode: true,
 }
