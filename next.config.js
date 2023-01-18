eca1fd85a171bdc91abe325dd437be4fbc78516b
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2023-01-17 23:15:13 -0500

    added redirect in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://main.d1j330gths82ly.amplifyapp.com/:path*',
+      },
+    ]
+  },
   images: {
     domains: ['links.papareact.com' , 'i.ibb.co', 'lh3.googleusercontent.com', 'cdn.sanity.io']
   },
