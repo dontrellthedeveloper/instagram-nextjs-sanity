52c21e49c5c2cc7aa888a5ab486d5ac308c19821
dontrellknight@gmail.com <dontrellknight@gmail.com>
2023-01-17 23:55:41 -0500

    updated page title and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,9 +16,18 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
       <>
+          {/*<Head>*/}
+          {/*    <title>Instagram | Dontrell Dev</title>*/}
+          {/*    <link rel="icon" href="/favicon.ico" />*/}
+          {/*</Head>*/}
+
+
           <Head>
               <title>Instagram | Dontrell Dev</title>
-              <link rel="icon" href="/favicon.ico" />
+              <link rel="shortcut icon" href="/favicon.png" />
+              <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
+              <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png"/>
+              <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png"/>
           </Head>
 
           <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
@@ -32,4 +41,4 @@ function MyApp({ Component, pageProps }: AppProps) {
   )
 }
 
-export default MyApp
+export default MyApp;
